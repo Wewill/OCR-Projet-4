29b324b20a208abdc53f3ccd6d82f961c7776c86
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2024-09-04 10:21:09 +0200

    Fix html errors & Ajouter validation ou messages d'erreur #3

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,14 +22,23 @@ const formData = document.querySelectorAll(".formData");
 const form = document.getElementById('form');
 
 // Validation functions
+function inputValidation(field) {
+  return (document.getElementById(field).value !== null) ? true : false;
+}
+
 function textValidation(field) {
- 	return (document.getElementById(field).value !== null && document.getElementById(field).value.length > 2) ? true : false;
+ 	return (document.getElementById(field).value !== null && document.getElementById(field).value.length > document.getElementById(field).getAttribute("minlength")) ? true : false;
 }
 
  function emailValidation(field) {
  	let regex = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/;
  	return regex.test(document.getElementById(field).value);
  }
+
+ function dateValidation(field) {
+  let regex = /^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[\/\-]\d{4}$/;
+  return regex.test(document.getElementById(field).value);
+}
 
  function quantityValidation(field) {
  	let regex = /^[0-9]+$/;
@@ -55,18 +64,54 @@ function textValidation(field) {
  		let isValid = true;
     // Validate
     if (!textValidation("first")) {
+      document.getElementById("first").parentNode.setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+      document.getElementById("first").parentNode.setAttribute("data-error-visible", true);
  			isValid = false;
- 		} else if (!textValidation("last")) {
+    } else 
+      document.getElementById("first").parentNode.setAttribute("data-error-visible", false);
+
+ 		if (!textValidation("last")) {
+      document.getElementById("last").parentNode.setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+      document.getElementById("last").parentNode.setAttribute("data-error-visible", true);
  			isValid = false;
- 		} else if (!emailValidation("email")) {
+    } else 
+      document.getElementById("last").parentNode.setAttribute("data-error-visible", false);
+
+    if (!dateValidation("birthdate")) {
+      document.getElementById("birthdate").parentNode.setAttribute("data-error", "Veuillez entrer une date de naissance.");
+      document.getElementById("birthdate").parentNode.setAttribute("data-error-visible", true);
+        isValid = false;
+    } else 
+      document.getElementById("birthdate").parentNode.setAttribute("data-error-visible", false);  
+
+ 		if (!emailValidation("email")) {
+      document.getElementById("email").parentNode.setAttribute("data-error", "Veuillez entrer un email valide.");
+      document.getElementById("email").parentNode.setAttribute("data-error-visible", true);
  			isValid = false;
- 		} else if (!quantityValidation("quantity")) {
+    } else 
+      document.getElementById("email").parentNode.setAttribute("data-error-visible", false);
+
+ 		if (!quantityValidation("quantity")) {
+      document.getElementById("quantity").parentNode.setAttribute("data-error", "Veuillez choisir une quantité entre 0 et 99.");
+      document.getElementById("quantity").parentNode.setAttribute("data-error-visible", true);
  			isValid = false;
- 		} else if (!locationValidation()) {
+    } else 
+    document.getElementById("quantity").parentNode.setAttribute("data-error-visible", false);
+
+ 		if (!locationValidation()) {
+      document.getElementById("locations").setAttribute("data-error", "Veuillez choisir une option.");
+      document.getElementById("locations").setAttribute("data-error-visible", true);
  			isValid = false;
- 		} else if (!checkboxValidation("checkbox1")) {
+    } else 
+      document.getElementById("locations").setAttribute("data-error-visible", false);
+
+ 		if (!checkboxValidation("checkbox1")) {
+      document.getElementById("checkbox1").parentNode.setAttribute("data-error", "Veuillez vérifier que vous acceptez les termes et conditions.");
+      document.getElementById("checkbox1").parentNode.setAttribute("data-error-visible", true);
  			isValid = false;
- 		}
+    } else 
+      document.getElementById("checkbox1").parentNode.setAttribute("data-error-visible", false);
+
     // Then submit 
  		if (isValid) {
       console.log("Form is valid > submit")
